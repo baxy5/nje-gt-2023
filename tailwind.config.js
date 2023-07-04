16cdb1d48a172b7d4787503996ca517a5ea76821
baxy5 <bakos.soda@gmail.com>
2023-07-04 22:26:54 +0200

    next template init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {},
+    fontFamily: {},
+    screens: {},
+    fontSize: {},
+    container: {
+      padding: {},
+      screens: {},
+    },
+    extend: {
+      keyframes: {},
+    },
+    animation: {},
+  },
+  plugins: [],
+};
