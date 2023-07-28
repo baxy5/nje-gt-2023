124c01bb39e631042e3ccc6469f1681061f52dfe
baxy5 <bakos.soda@gmail.com>
2023-07-28 16:39:37 +0200

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1 className="uppercase underline">Home</h1>
+      <h1 className="uppercase underline">nje gt 2023</h1>
     </div>
   );
 };
