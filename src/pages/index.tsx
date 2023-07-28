e89400b759795def83b38f59a28a495469f6c336
baxy5 <bakos.soda@gmail.com>
2023-07-28 17:25:07 +0200

    database setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import prisma from '../../lib/prisma';
+import { GetStaticProps } from 'next';
 
-const Home = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const csapatok = await prisma.csapat.findMany();
+  return {
+    props: { csapatok },
+    revalidate: 10,
+  };
+};
+//npx prisma studio
+const Home = ({csapatok}) => {
+  useEffect(() => {
+    console.log(csapatok)
+  })
+
   return (
     <div>
       <h1 className="uppercase underline">nje gt 2023</h1>
+      <p>{csapatok[0].name}</p>
     </div>
   );
 };
