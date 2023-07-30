c047ac02c5e75a857ff2fbc10281501301df2fc4
baxy5 <bakos.soda@gmail.com>
2023-07-30 18:08:43 +0200

    logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import prisma from '../../lib/prisma';
 import { GetStaticProps } from 'next';
 import RankingList from '../../components/RankingList';
+import Image from 'next/image';
+
+import logo from '../../public/hoklogo.png';
 
 export const getStaticProps: GetStaticProps = async () => {
   const csapatok = await prisma.csapat.findMany({
@@ -18,7 +21,8 @@ export const getStaticProps: GetStaticProps = async () => {
 const Home = ({ csapatok }) => {
   return (
     <div>
-      <div className="bg-[#F4BB44] py-4">
+      <div className="bg-[#F4BB44] py-4 flex flex-col justify-center items-center">
+        <Image src={logo} width={150} height={150} alt='logo' />
         <h1 className="text-[2.5rem] text-center font-bold">NJE GT 2023</h1>
         <p className="text-[1.5rem] text-center">Ranglista</p>
       </div>
